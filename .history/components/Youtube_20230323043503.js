f72ad2e0a4cc3b7e686c803960dab3331c63ae19
Manuel <mlab33972@gmail.com>
2023-03-23 05:03:00 -0400

    Add new Articles

---
+++ .history/components/Youtube_20230323043503.js
@@ -0,0 +1,14 @@
+
+
+export default function YouTube({ id }) {
+  return (
+    <div className="container__youtube">
+      <iframe
+        src={`https://www.youtube.com/embed/${id}`}
+        allow="autoplay; encrypted-media"
+        title="Embedded YouTube video"
+        className="frame"
+      />
+    </div>
+  );
+}
