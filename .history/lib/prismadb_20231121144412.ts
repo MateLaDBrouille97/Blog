fbe5da362096dbcc64f6e40c2252cc5c8a5c382f
Manuel <mlab33972@gmail.com>
2023-11-21 17:05:40 -0500

    update

--- .history/lib/prismadb_20231121144412.ts
+++ .history/lib/prismadb_20231121144412.ts
@@ -12,7 +12,7 @@ const prismadb = globalThis.prisma || new PrismaClient({
     },
   },
 })
-// if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb
 
 export default prismadb;
 
