228381227b6f6d0fe9279560e39290d07b1447b6
Manuel <mlab33972@gmail.com>
2023-11-21 16:48:08 -0500

    update

--- .history/config/index_20231121141525.js
+++ .history/config/index_20231121141525.js
@@ -3,8 +3,6 @@ export const API_URL =
 //checks to see if its hosted on another website or localhost:1337 (strapi)
 
 export const NEXT_URL =
-"https://main.d6iszn1o7sirg.amplifyapp.com" ||
-  // process.env.NEXT_PUBLIC_FRONTEND_URL || 
-  "http://127.0.0.1:3000";
+  process.env.NEXT_PUBLIC_FRONTEND_URL || "http://127.0.0.1:3000";
 
 export const PER_PAGE = 5; //global const for number of events per page
